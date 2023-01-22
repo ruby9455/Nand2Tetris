2f54a3c8a87030486a6a82aaca083ff2b0c1ebe7
ruby9455 <64198767+ruby9455@users.noreply.github.com>
2023-01-22 12:30:59 +1030

    Create SymbolTable.java

---
+++ SymbolTable.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class SymbolTable {
+
+    /**
+     * Constructor for the Symbol table
+     */
+    public SymbolTable() {
+        // Your code here
+    }
+
+    /**
+     * Adds a symbol to the symbol table
+     * 
+     * @param symbol The name of the symbol
+     * @param value  The address for the symbol
+     */
+    public static void addSymbol(String symbol, int value) {
+        // Your code here
+
+    }
+
+    /**
+     * Gets a symbol from the symbol table
+     * 
+     * @param symbol The name of the symbol
+     * @return The address for the symbol or -1 if the symbol isn't in the table
+     */
+    public static int getSymbol(String symbol) {
+        // Your code here
+        return -1;
+    }
+
+}
